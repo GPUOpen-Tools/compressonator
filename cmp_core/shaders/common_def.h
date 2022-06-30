95864c0058038c918d1b9ca8b661a30e547acf81
Aaron Barany <akb825@gmail.com>
2022-06-30 11:27:54 -0700

    Fixed build of cmp_core on BSD. (#208)

--- cmp_core/shaders/common_def.h
+++ cmp_core/shaders/common_def.h
@@ -105,7 +105,7 @@
 #define INC_cmp_math_func "cmp_math_func.h"
 #endif
 
-#if defined(__linux__) || defined(__APPLE__)
+#if defined(__unix__) || defined(__APPLE__)
 #ifndef _LINUX
 #define _LINUX
 #endif
